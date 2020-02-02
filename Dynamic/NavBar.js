12b60d5cdee2396c1851a05e1d7ec416546a02ac
Andrew Rotert <Andrew.Rotert@trojans.dsu.edu>
2020-02-02 12:10:34 -0600

    1.0.2
    Added bookmark link, changed navigation bar.

---
+++ Dynamic/NavBar.js
@@ -0,0 +1,14 @@
+﻿window.onscroll = function () { myFunction() };
+
+var navbar = document.getElementById("topnavbar");
+
+
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky")
+    } else {
+        navbar.classList.remove("sticky");
+    }
+}
