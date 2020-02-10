06906dd8cbd82f56a76d1674774ecf465bdc843f
ajrotert <andrew.rotert@trojans.dsu.edu>
2020-02-10 12:50:03 -0600

    1.1.0

---
+++ Dynamic/loadprojects.js
@@ -0,0 +1,14 @@
+﻿$(function () {
+
+    if (document.getElementById("iOS-Table") != null) {
+        $("#iOS-Table").load("Dynamic/HTML/iOSPlaceholder.html");
+    }
+
+    if (document.getElementById("Android-Table") != null) {
+        $("#Android-Table").load("Dynamic/HTML/AndroidPlaceholder.html");
+    }
+    
+    if (document.getElementById("Other-Table") != null) {
+        $("#Other-Table").load("Dynamic/HTML/OtherPlaceholder.html");
+    }
+});
