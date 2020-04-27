5f9a90993635445ae04435a607fe9d9ba719d4d2
ajrotert <andrew.rotert@trojans.dsu.edu>
2020-04-27 15:10:41 -0500

    Cleaning code up

--- Dynamic/Scripts/PageLoader.js
+++ Dynamic/Scripts/PageLoader.js
@@ -77,35 +77,3 @@ var events = function(){
     else 
         window.onmousemove = null;
 };
-
-//document.getElementById("Android-Table").onclick = function () {
-
-
-
-//    };
-
-
-
-   /* <div class="grid-container">
-            <div class="block_region">
-                <a class="overlay_region" href="#"></a>
-                <div class="inner_region">
-                    <center>
-                        <img class="resizing" src="Images/Projects/Lift.png" alt="Lift Force Calculator" />
-                        <h2><i>Lift Force Calculator</i></h2>
-                        <p>Published: <a href="LiftCoefficientCalculator.html">Support Page</a></p>
-                        <p>View at the Google Play Store: <a href="https://play.google.com/store/apps/details?id=com.ar.liftcalculator">Visit Now</a></p>
-                        <p>Language Used: C#</p>
-                        <p>About:</p>
-                        <p class="just">
-                            Lift Coefficient Calclulator uses an Android based platform to calculate the lift coefficient formula.
-                            The user interface implaments event handlers for each text field, allowing calculations to happen
-                            as soon as all the required input is provided.
-    
-                    </p>
-                        <hr class="table" />
-                    </center>
-                </div>
-            </div>
-    </div>
-    */
