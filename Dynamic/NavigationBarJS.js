b574f0fc4feb10e003124c3412c3c2f59f38ae47
ajrotert <andrew.rotert@trojans.dsu.edu>
2020-06-08 22:21:10 -0500

    Updated data, fixed error with loading content

--- Dynamic/NavigationBarJS.js
+++ Dynamic/NavigationBarJS.js
@@ -1,4 +1,12 @@
-﻿$(function () {
-    $("#nav-placeholder").load("Dynamic/NavigationBar.html");
+﻿var loadFunction = function () {
+    $("#nav-placeholder").load("Dynamic/NavigationBar.html", function (response, status, xhr) {
+        if (status == "error") {
+            console.log("An error has occured. Attempting to reload navigation bar.");
+            caches.delete("NavigationBar.html");
+            caches.delete("Dynamic/NavigationBar.html");
+            loadFunction();
+        }
+    });
 
-});
+};
+$(loadFunction);
--- Dynamic/loadprojects.js
+++ Dynamic/loadprojects.js
@@ -1,13 +1,44 @@
-﻿$(function () {
+﻿var loadIos = function () {
+    $("#iOS-Table").load("Dynamic/HTML/iOSPlaceholder.html", function (response, status, xhr) {
+        if (status == "error") {
+            console.log("An error has occured. Attempting to reload iOS projects.");
+            caches.delete("iOSPlaceholder.html");
+            caches.delete("Dynamic/HTML/iOSPlaceholder.html");
+            loadIos();
+        }
+    });
+};
+var loadAndroid = function () {
+    $("#Android-Table").load("Dynamic/HTML/AndroidPlaceholder.html", function (response, status, xhr) {
+        if (status == "error") {
+            console.log("An error has occured. Attempting to reload Android projects.");
+            caches.delete("AndroidPlaceholder.html");
+            caches.delete("Dynamic/HTML/AndroidPlaceholder.html");
+            loadAndroid();
+        }
+    });
+};
+var loadOther = function () {
+    $("#Other-Table").load("Dynamic/HTML/OtherPlaceholder.html", function (response, status, xhr) {
+        if (status == "error") {
+            console.log("An error has occured. Attempting to reload other projects.");
+            caches.delete("OtherPlaceholder.html");
+            caches.delete("Dynamic/HTML/OtherPlaceholder.html");
+            loadOther();
+        }
+    });
+};
+
+$(function () {
     if (document.getElementById("iOS-Table") != null) {
-        $("#iOS-Table").load("Dynamic/HTML/iOSPlaceholder.html");
+        loadIos();
     }
 
     if (document.getElementById("Android-Table") != null) {
-        $("#Android-Table").load("Dynamic/HTML/AndroidPlaceholder.html");
+        loadAndroid();
     }
     
     if (document.getElementById("Other-Table") != null) {
-        $("#Other-Table").load("Dynamic/HTML/OtherPlaceholder.html");
+        loadOther();
     }
 });
