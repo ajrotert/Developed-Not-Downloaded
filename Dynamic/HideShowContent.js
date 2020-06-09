bdd56285b0a2cd60ee3aeed420db6e112b67e925
ajrotert <andrew.rotert@trojans.dsu.edu>
2020-06-09 16:27:25 -0500

    Added animation to about me page

--- Dynamic/HideShowContent.js
+++ Dynamic/HideShowContent.js
@@ -1,4 +1,5 @@
-﻿function toggleContent(event) {
+﻿var intervalID;
+function toggleContent(event) {
     var ids = document.getElementById(event.currentTarget.id);
     ids = document.getElementById(ids.id.replace("-carrot", ""));
     var contents = ids;
@@ -6,10 +7,28 @@
 
     if (contents.style.display === "none") {
         contents.style.display = "inline-block";
-        console.log("here: " + carrots.getElementsByTagName("p").textContent);
+        contents.style.opacity = 0;
+        intervalID = setInterval(buildOpacity, 15, contents);
         carrots.innerHTML = "<center> <p>&#x2571&#x2572</p> </center>";
     } else {
-        contents.style.display = "none";
+        contents.style.opacity = 1;
+        intervalID = setInterval(dropOpacity, 15, contents);
         carrots.innerHTML = "<center> <p>&#x2572&#x2571</p> </center>";
     }
-}
+};
+
+function buildOpacity(node) {
+    var opacity = parseFloat(node.style.opacity) + .05;
+    node.style.opacity = opacity;
+    if (node.style.opacity >= 1.0) {
+        clearInterval(intervalID);
+    }
+};
+function dropOpacity(node) {
+    var opacity = parseFloat(node.style.opacity) - .05;
+    node.style.opacity = opacity;
+    if (node.style.opacity <= 0) {
+        clearInterval(intervalID);
+        node.style.display = "none";
+    }
+};
