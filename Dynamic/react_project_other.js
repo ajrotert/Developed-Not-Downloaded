bda6266ac85f3d423e392886bbc1ff13d047d30d
ajrotert <andrew.rotert@trojans.dsu.edu>
2020-06-27 00:52:18 -0500

    Updated Components using react

---
+++ Dynamic/react_project_other.js
@@ -0,0 +1,198 @@
+﻿class OtherProjects extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
+
+    render() {
+        return (
+            <div className="grid-container">
+                <div className="block_region ood-tag advanced-tag">
+                    <a className="overlay_region"></a>
+                    <div className="inner_region">
+                        <center>
+                            <img className="resizing" src="Images/Projects/GripFinder2.png" alt="Grip Finder" />
+                            <h2><i>Grip Finder</i></h2>
+                            <p>See the code: <a href="https://github.com/ajrotert/Grip-Finder">View Repository</a></p>
+                            <p>Tags:  <object className="tag">Object Oriented Design</object> <object className="tag">Advanced Algorithms</object></p>
+                            <p>Language Used: C#</p>
+                            <p>About:</p>
+                            <p className="just">
+                                Grip Finder is a Windows Form Application. It is desiged based off another project, and is based
+                                around Object Oriented Design. Grip Finder works by taking five inputs, and their importance
+                                and outputs a golf grip recommendation.
+                            </p>
+
+                            <hr className="table" />
+                        </center>
+                    </div>
+                </div>
+                <div className="block_region ood-tag advanced-tag">
+                    <a className="overlay_region"></a>
+                    <div className="inner_region">
+                        <center>
+                            <img className="resizing" src="Images/Projects/Minesweeper.png" alt="Minesweeper" />
+                            <h2><i>Minesweeper</i></h2>
+                            <p>See the code: <a href="https://github.com/ajrotert/AR.Minesweeper">View Repository</a></p>
+                            <p>Tags:  <object className="tag">Object Oriented Design</object> <object className="tag">Advanced Algorithms</object></p>
+                            <p>Language Used: C#</p>
+                            <p>About:</p>
+                            <p className="just">
+                                Minesweeper uses some basic Object Oriented Design, and dynamic layouts to create a minesweeper game.
+                                This Windows Form Application uses a flood fill algorithm to queue and dequeue blocks until the queue
+                                is empty.
+                            </p>
+
+                            <hr className="table" />
+                        </center>
+                    </div>
+                </div>
+                <div className="block_region regex-tag team-tag">
+                    <a className="overlay_region"></a>
+                    <div className="inner_region">
+                        <center>
+                            <img className="resizing" src="Images/Projects/Tsharp.png" alt="Tsharp" />
+                            <h2><i>Tsharp</i></h2>
+                            <p>See the code: <a href="https://github.com/ajrotert/Language-Processing">View Repository</a></p>
+                            <p>Tags: <object className="tag">Regular Expressions</object> <object className="tag">Team Collaboration</object></p>
+                            <p>Language Used: Yacc, Lex, C</p>
+                            <p>About:</p>
+                            <p className="just">
+                                Tsharp is designed to do simple math calculations and graphs. Tsharp uses regular expressions to create a new simple
+                                programming language. The key words from the Tsharp
+                                language are translated and compiled to C, creating an executable file.
+                            </p>
+
+                            <hr className="table" />
+                        </center>
+                    </div>
+                </div>
+                <div className="block_region stack-tag">
+                    <a className="overlay_region"></a>
+                    <div className="inner_region">
+                        <center>
+                            <img className="resizing" src="Images/Projects/Golf2.png" alt="Mini-Golf" />
+                            <h2><i>Mini-Golf</i></h2>
+                            <p>See the code: <a href="https://github.com/ajrotert/Assembely">View Repository</a></p>
+                            <p>Tags: <object className="tag">Stack</object></p>
+                            <p>Language Used: Assembly x86</p>
+                            <p>About:</p>
+                            <p className="just">
+                                Mini-Golf is a command line game writen completely in assembly. The game takes a power level and angle
+                                as input. The assembly code has to load the angle as a floating point to calculate the right angle. The
+                                game ends when the '.' gets to the 0.
+                            </p>
+
+                            <hr className="table" />
+                        </center>
+                    </div>
+                </div>
+                <div className="block_region hosted-tag database-tag firebase-tag functions-tag">
+                    <a className="overlay_region"></a>
+                    <div className="inner_region">
+                        <center>
+                            <img className="resizing" src="Images/Projects/Website2.png" alt="Website" />
+                            <h2><i>Developed Not Download</i></h2>
+                            <p>See the code: <a href="https://github.com/ajrotert/Developed-Not-Downloaded">View Repository</a></p>
+                            <p>Tags: <object className="tag">Hosted</object> <object className="tag">Database Integration</object> <object className="tag">Google Firebase</object> <object className="tag">Cloud Functions</object></p>
+                            <p>Language Used: HTML5, CSS3, JavaScript</p>
+                            <p>About:</p>
+                            <p className="just">
+                                Developed Not Downloaded is a series of static webpages designed with HTML and CSS. It is my main website to
+                                showcase my projects, and provide support to my published applications. Also hosts my App API project, which
+                                uses a google cloud funtions to access Apple's API and display the number of downloads on my iOS apps.
+                            </p>
+
+                            <hr className="table" />
+                        </center>
+                    </div>
+                </div>
+                <div className="block_region hosted-tag database-tag firebase-tag">
+                    <a className="overlay_region"></a>
+                    <div className="inner_region">
+                        <center>
+                            <img className="resizing" src="Images/Projects/productbacklogdev.png" alt="Website" />
+                            <h2><i>Product Backlog Developer</i></h2>
+                            <p>Live Demonstration: <a href="https://productbacklog.dev">Visit Now</a></p>
+                            <p>See the code: <a href="https://github.com/ajrotert/productbacklog">View Repository</a></p>
+                            <p>Tags: <object className="tag">Hosted</object> <object className="tag">Database Integration</object> <object className="tag">Google Firebase</object> </p>
+                            <p>Language Used: HTML5, CSS3, JavaScript, React</p>
+                            <p>About:</p>
+                            <p className="just">
+                                Product Backlog Developer is a dynamic web application. It is built using a firebase backend, and react frontend. Users can sign in through the firebase
+                                authentication UI, and create their own product backlog. Users can add stories or defects to their backlog, remove stories or defects, and complete backlog items.
+                                Realtime database access allows multiple users to manage the same backlog.
+                            </p>
+
+                            <hr className="table" />
+                        </center>
+                    </div>
+                </div>
+                <div className="block_region ood-tag thread-tag">
+                    <a className="overlay_region"></a>
+                    <div className="inner_region">
+                        <center>
+                            <img className="resizing" src="Images/Projects/ClientHost.png" alt="Website" />
+                            <h2><i>Client / Host Communication</i></h2>
+                            <p>See the code: <a href="https://github.com/ajrotert/BasicClientServerConnections">View Repository</a></p>
+                            <p>Tags: <object className="tag">Object Oriented Design</object> <object className="tag">Multithreaded</object></p>
+                            <p>Language Used: Java</p>
+                            <p>About:</p>
+                            <p className="just">
+                                Project exploring concepts talked about in Operating Systems. This project has two main parts, the Host and the Client JavaFX programs.
+                                The host allows for multiple clients to connect and pass string messages back and forth. This process is broken down in to many threads, to handle
+                                input and output simultaneously.
+                            </p>
+                            <hr className="table" />
+
+                        </center>
+                    </div>
+                </div>
+                <div className="block_region hosted-tag machine-tag team-tag">
+                    <a className="overlay_region"></a>
+                    <div className="inner_region">
+                        <center>
+                            <img className="resizing" src="Images/Projects/NumericDetector.png" alt="Website" />
+                            <h2><i>Numeric Detector</i></h2>
+                            <p>Live Demonstration: <a href="https://codepython.life">Visit Now</a></p>
+                            <p>See the code: <a href="https://github.com/ajrotert/NumberDetection">View Repository</a></p>
+                            <p>Tags: <object className="tag">Hosted</object> <object className="tag">Machine Learning</object> <object className="tag">Team Collaboration</object></p>
+                            <p>Language Used: JavaScript, HTML5, CSS3</p>
+                            <p>About:</p>
+                            <p className="just">
+                                Number Detector creates and uses a machine learning model to predict a hand written number. The model is trained using the MNIST
+                                dataset, and is retrained every time the webpage loads. The model trains and runs locally, using javascript.
+                            </p>
+                            <hr className="table" />
+
+                        </center>
+                    </div>
+                </div>
+                <div className="block_region advanced-tag team-tag">
+                    <a className="overlay_region"></a>
+                    <div className="inner_region">
+                        <center>
+                            <h2><i>Programming Competitions</i></h2>
+                            <p>Tags: <object className="tag">Advanced Algorithms</object> <object className="tag">Team Collaboration</object></p>
+                            <p>Language Used: C++, Python, C# </p>
+                            <p>About:</p>
+
+                                <ul style={{textAlign: "left"}}> Programming Competitions I have competed in: <br/><br/>
+                                    <li style={{textAlign: "left"}}>2019 ACM Regional</li>
+                                    <li style={{textAlign: "left"}}>DSU Local Programming Competitions</li>
+                                    <li style={{textAlign: "left"}}>MICS Programming Competition in Fargo</li>
+                                    <li style={{textAlign: "left"}}>Google Hashcode</li>
+                                    <li style={{textAlign: "left"}}>Google Codejam</li>
+                                    <li style={{textAlign: "left"}}>Etc.</li>
+                                </ul>
+
+                            <hr className="table" />
+                        </center>
+                    </div>
+                </div>
+            </div>
+        ); 
+    }
+}
+
+ReactDOM.render(<OtherProjects />, document.querySelector('#Other-Table'));
